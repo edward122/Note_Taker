8bc6da9e83fd149dbd9e34eeac420264eed52852
Sky Market <kisiledw12345@gmail.com>
2025-04-03 00:01:13 -0700

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAAyPbgbNVAyPOecFKHWVq5uwDMaKyoM80",
-  authDomain: "mind-map3.firebaseapp.com",
-  projectId: "mind-map3",
-  storageBucket: "mind-map3.firebasestorage.app",
-  messagingSenderId: "795917459686",
-  appId: "1:795917459686:web:ed4cc63b613e74aa725b27",
-  measurementId: "G-TDK7SLFM89"
+  apiKey: "AIzaSyBwhJVoZ2nKnGrl9VJ7D6GXpcUha2IRN08",
+  authDomain: "notetaker-f71ae.firebaseapp.com",
+  databaseURL: "https://notetaker-f71ae-default-rtdb.firebaseio.com",
+  projectId: "notetaker-f71ae",
+  storageBucket: "notetaker-f71ae.firebasestorage.app",
+  messagingSenderId: "466022334149",
+  appId: "1:466022334149:web:bb9e6830f35a4b94b73aac",
+  measurementId: "G-ZH0WYZKWKN"
 };
 
 // Initialize Firebase
