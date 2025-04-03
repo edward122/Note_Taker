66b35c66529bb4d6d5dce12b75054a317a1531b6
Sky Market <kisiledw12345@gmail.com>
2025-04-02 23:37:37 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/Note_Taker/', // Matches your GitHub Pages repository name
+   base: '/', // Matches your GitHub Pages repository name
    plugins: [react()]
 })
